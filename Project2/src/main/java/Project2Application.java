7c7d11995f1dfe2f8559477d8055bc99e2d2a688
Samuel Feye <samfeye@gmail.com>
2020-10-18 14:06:03 -0500

    Initial commit

--- Project2/src/main/java/Project2Application.java
+++ Project2/src/main/java/Project2Application.java
@@ -1,6 +1,36 @@
+import service.MCTS;
+import utils.Board;
+import utils.Position;
+
+import java.util.Scanner;
+
 public class Project2Application {
 
-    public void main(String[] args) {
-        
+    public static void main(String[] args) {
+        Board board = new Board();
+        MCTS cpu = new MCTS ();
+        board.printBoard();
+
+        while (board.checkStatus() == -1) {
+
+            //Player 1 play
+            Scanner sc = new Scanner(System.in);
+            System.out.println( "\n" + "Player 1 select a row to play...");
+            int col = sc.nextInt();
+            Position pos = new Position(board.getLowest(col), col);
+            board.performMove(1, pos);
+            board.printBoard();
+
+            System.out.println("\n" + "_____________" + "\n");
+
+            if(board.checkStatus() == -1) {
+
+                //CPU play
+                board = cpu.findNextMove(board, 2);
+                board.printBoard();
+            }
+        }
+
+        board.printStatus();
     }
 }
